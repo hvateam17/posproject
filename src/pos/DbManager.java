a7784af0c870b7a7f54be4f7c1eaae707b46c353
hvateam17 <benoitwdc@gmail.com>
2013-11-22 00:23:05 +0100

    TEST123456

--- src/pos/DbManager.java
+++ src/pos/DbManager.java
@@ -13,7 +13,7 @@ public class DbManager {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             String url = "jdbc:mysql://localhost/pos";
-            String user = "ben", pass = "loveyou12345";
+            String user = "ben", pass = "loveyou123456";
 
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException e) {
